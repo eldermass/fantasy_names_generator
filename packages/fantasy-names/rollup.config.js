56e1d694da372e82b09d3e1dafdd26b7b7514c46
chenyue <285653184@qq.com>
2022-08-17 22:09:49 +0800

    chore: add rollup setting

--- packages/fantasy-names/rollup.config.js
+++ packages/fantasy-names/rollup.config.js
@@ -1,18 +1,16 @@
-// rollup.config.js
-// 1. 这里使用到的安装包均需要安装成开发依赖
-// 例如下面用到需要如下安装
 // yarn add -D rollup rollup-plugin-babel @rollup/plugin-typescript @rollup/plugin-node-resolve
 
 // 导出defineConfig方法可以让编辑器（VSCode）智能提示所有的rollup的配置项，很方便
 import { defineConfig } from "rollup"
-// 这里是babel的插件，用来处理es的转换，当然会用一个.babelrc配置文件，下面也会简单列出来
+// 这里是babel的插件，用来处理es的转换，当然会用一个.babelrc配置文件
 import babel from "rollup-plugin-babel"
 // rollup处理typescript的插件
 import typescript from "@rollup/plugin-typescript"
 // resolve将我们编写的源码与依赖的第三方库进行在之前的文章里面也有提到但是这里使用的@rollup/plugin-node-resolve
 import resolve from "@rollup/plugin-node-resolve"
-// 解决rollup.js无法识别CommonJS模块，这里使用的是@rollup/plugin-commonjs并不是之前提到的rollup-plugin-commonjs
-import commonjs from "@rollup/plugin-commonjs"
+// 解决rollup.js无法识别CommonJS模块，这里使用的是@rollup/plugin-commonjs
+// import commonjs from "@rollup/plugin-commonjs"
+import json from '@rollup/plugin-json'
 // 引入package.json
 import pkg from "./package.json"
 
@@ -22,17 +20,22 @@ export default defineConfig({
     input: "src/index.ts",
     output: [
         {
-            file: `dist/${libName}.cjs.js`,
+            file: `lib/index.cjs.js`,
             // commonjs格式
             format: "cjs",
         },
         {
-            file: `dist/${libName}.es.js`,
+            file: `lib/index.es.js`,
             // es module
             format: "es",
         },
         {
-            file: `dist/${libName}.umd.js`,
+            file: 'lib/index.js',
+            format: "umd",
+            name: libName,
+        },
+        {
+            file: `lib/index.umd.js`,
             // 通用格式可以用于node和browser等多个场景
             format: "umd",
             // 外部引入的模块需要显式告知使用的三方模块的命名，结合下面的external使用
@@ -51,5 +54,6 @@ export default defineConfig({
             sourceMap: false,
         }),
         resolve(),
+        json()
     ],
 })
