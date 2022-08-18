8f5c26e790ec330bcf41d553f768c1053b28338d
chenyue <285653184@qq.com>
2022-08-18 18:47:52 +0800

    feat: add mini programe app

---
+++ apps/mini/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+import { getName } from '@chenxuanyu/fantasy-names'
+onLaunch(() => {
+  console.log("App Launch");
+  let a = getName(10)
+  console.log(a)
+});
+onShow(() => {
+  console.log("App Show");
+});
+onHide(() => {
+  console.log("App Hide");
+});
+</script>
+<style>
+</style>
